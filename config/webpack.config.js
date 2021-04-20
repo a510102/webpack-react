efebee71017120e28b0392dce47dd18d6917d16e
JayHung123 <jah@oglobal.com>
2021-04-20 18:14:51 +0800

    [UPDATE] 1.update the webpack setting

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin'); 
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: {
-    main: path.resolve(__dirname, '../src', 'main.js'),
-  },
+  entry: [
+    '@babel/polyfill',
+    path.resolve(__dirname, '../src', 'main.js')
+  ],
   output: {
     path: path.resolve(__dirname, '../dist'),
     filename: '[name].[hash:8].js',
@@ -14,8 +16,70 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/, //css
+        use: [
+          MiniCssExtractPlugin.loader, 
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i, //圖片
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10240,
+              fallback: {
+                loader: 'file-loader',
+                options: {
+                  name: 'image/[name].[hash].[ext]',
+                },
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/, //多媒體
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10240,
+              fallback: {
+                loader: 'file-loader',
+                options: {
+                  name: 'media/[name].[hash:8].[ext]',
+                },
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i, //字型
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10240,
+              fallback: {
+                loader: 'file-loader',
+                options: {
+                  name: 'fonts/[name].[hash:8].[ext]',
+                },
+              },
+            },
+          },
+        ],
       },
     ],
   },
@@ -26,5 +90,9 @@ module.exports = {
       scriptLoading: 'blocking'
     }),
     new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].[hash].css",
+      chunkFilename: "[id].css",
+    }),
   ],
 };
--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Home from './pages/Home'
 import './assets/reset.css';
 
-console.log('all');
+ReactDOM.render(
+  <Home />,
+  document.getElementById('root')
+)
